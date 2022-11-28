6a1c7304840b9f321b947a27a1c4f73c78944793
RyoSogawa <koashimitekara.1122@gmail.com>
2022-11-28 22:40:39 +0900

    add qwik Button component

---
+++ qwik/src/components/Button/index.tsx
@@ -0,0 +1,18 @@
+import { component$, useStore, useClientEffect$, Slot } from '@builder.io/qwik';
+
+const Button = component$(() => {
+  const store = useStore({count: 0});
+
+  useClientEffect$(() => {
+    console.log('mounted');
+  })
+
+  return (
+    <button type={'button'} onClick$={() => store.count++}>
+      <Slot/>
+      <span>:{store.count}</span>
+    </button>
+  );
+});
+
+export default Button;
